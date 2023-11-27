ded1188d08fdd14ee75d1953a4b4f537a89120ec
Krisztián Veress <veresskrisztian@gmail.com>
2023-11-27 09:54:19 +0000

    Initial skeleton commit

---
+++ src/main/java/hu/bankmonitor/mortgage/MortgageApplication.java
@@ -0,0 +1,13 @@
+package hu.bankmonitor.mortgage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MortgageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MortgageApplication.class, args);
+	}
+
+}
